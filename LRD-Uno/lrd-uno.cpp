d4f7e761b6df085b88d721b1adc3dbb53c7a2082
Clara Vas <vasconcelosousa2508@gmail.com>
2026-04-29 09:32:26 -0300

    Add LDR-based buzzer control functionality
    Implement LDR and potentiometer functionality to control buzzer frequency based on light levels.

---
+++ LRD-Uno/lrd-uno.cpp
@@ -0,0 +1,29 @@
+// Definições de pinos
+const int pinoLDR = A0;
+const int pinoPot = A1;
+const int pinoBuzzer = 9;
+
+void setup() {
+  pinMode(pinoBuzzer, OUTPUT);
+  Serial.begin(9600); // ver os números no PC
+}
+
+void loop() {
+  int leituraLuz = analogRead(pinoLDR);
+  int ajusteSensibilidade = analogRead(pinoPot);
+
+  // MATEMÁTICA: Mapeia a leitura (ajustada pelo potenciômetro) 
+  // para frequências musicais (Ex: 261Hz [Dó] a 523Hz [Dó oitava])
+  // O valor 1023 é o máximo de luz, conforme você faz sombra, o valor cai.
+  
+  int frequencia = map(leituraLuz, 0, ajusteSensibilidade, 261, 523);
+
+  // Filtro simples para não tocar som se houver muita luz (mão longe)
+  if (leituraLuz < ajusteSensibilidade) {
+    tone(pinoBuzzer, frequencia);
+  } else {
+    noTone(pinoBuzzer);
+  }
+
+  delay(10); // Estabilidade do sistema
+}
