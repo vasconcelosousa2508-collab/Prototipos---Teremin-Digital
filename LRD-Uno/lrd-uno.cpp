579f9c400c3f6321e655f5018e41b6f07455a597
Clara Vas <vasconcelosousa2508@gmail.com>
2026-05-01 18:32:34 -0300

    Refactor comments and clean up code formatting

--- LRD-Uno/lrd-uno.cpp
+++ LRD-Uno/lrd-uno.cpp
@@ -5,25 +5,25 @@ const int pinoBuzzer = 9;
 
 void setup() {
   pinMode(pinoBuzzer, OUTPUT);
-  Serial.begin(9600); // ver os números no PC
+  Serial.begin(9600); 
 }
 
 void loop() {
   int leituraLuz = analogRead(pinoLDR);
   int ajusteSensibilidade = analogRead(pinoPot);
 
-  // MATEMÁTICA: Mapeia a leitura (ajustada pelo potenciômetro) 
+  // Mapeia a leitura (ajustada pelo potenciômetro) 
   // para frequências musicais (Ex: 261Hz [Dó] a 523Hz [Dó oitava])
-  // O valor 1023 é o máximo de luz, conforme você faz sombra, o valor cai.
+  // O valor 1023 é o máximo de luz, conforme a sombra, o valor cai.
   
   int frequencia = map(leituraLuz, 0, ajusteSensibilidade, 261, 523);
 
-  // Filtro simples para não tocar som se houver muita luz (mão longe)
+  // Filtro simples para não tocar som se houver muita luz
   if (leituraLuz < ajusteSensibilidade) {
     tone(pinoBuzzer, frequencia);
   } else {
     noTone(pinoBuzzer);
   }
 
-  delay(10); // Estabilidade do sistema
+  delay(10); // Estabilidade 
 }
