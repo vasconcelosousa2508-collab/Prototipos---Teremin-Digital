82bc7b46f5b49ac91407582320d7b81227502b5e
Clara Vas <vasconcelosusa2508@gmail.com>
2026-05-04 21:10:02 -0300

    Refatora código para melhorar a configuração da porta serial e a estrutura do loop de leitura

--- EstudosPy/testes_pyserial.py
+++ EstudosPy/testes_pyserial.py
@@ -1,13 +1,13 @@
 # Arduino está enviando dados da leitura de sombra do LRD.
 
-import serial
+# Configurações da Serial
+try:
+    porta = serial.Serial('/dev/ttyACM0', 9600, timeout=0.1)
+except:
+    print("Erro: Verifique se o Arduino está na porta /dev/ttyACM0")
+    exit()
 
-import numpy as np
-import sounddevice as sd
-from pynput import keyboard
-
-porta = serial.Serial('/dev/ttyACM0', 9600)
-
+# Configurações de Áudio
 amostragem = 44100
 frequencia = 440.0
 volume_atual = 0.0  
@@ -15,24 +15,21 @@ fase = 0
 
 limiteSombra = 400
 
-
 def audio_callback(outdata, frames, time, status):
     global fase, volume_atual
-    
     indices = np.arange(frames) + fase
-    
+    # Gerando a onda
     outdata[:, 0] = volume_atual * np.sin(2 * np.pi * frequencia * indices / amostragem)
-    
     fase += frames
 
 
-while(1):
+while True:
     dados = porta.readline()
     print(dados.decode('utf-8'))
 
-    if dados < limiteSombra:
+    if int(dados) < limiteSombra:
         volume_atual = 2.0
-            with sd.OutputStream(channels=1, callback=audio_callback, samplerate=amostragem):
-    else:
-        volume_atual = 0.0
-    
+    else:        volume_atual = 0.0
+
+    with sd.OutputStream(channels=1, callback=audio_callback, samplerate=amostragem):
+        pass
