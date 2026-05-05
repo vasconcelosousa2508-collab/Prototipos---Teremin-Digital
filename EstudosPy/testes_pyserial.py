77c86ec1ad3a71aad744495e1e6ae9e5a81802ed
Clara Vas <vasconcelosusa2508@gmail.com>
2026-05-04 21:04:06 -0300

    Adiciona lógica de controle de volume com base na leitura do LRD e ajusta a estrutura do código

--- EstudosPy/testes_pyserial.py
+++ EstudosPy/testes_pyserial.py
@@ -1,3 +1,5 @@
+# Arduino está enviando dados da leitura de sombra do LRD.
+
 import serial
 
 import numpy as np
@@ -6,6 +8,31 @@ from pynput import keyboard
 
 porta = serial.Serial('/dev/ttyACM0', 9600)
 
+amostragem = 44100
+frequencia = 440.0
+volume_atual = 0.0  
+fase = 0
+
+limiteSombra = 400
+
+
+def audio_callback(outdata, frames, time, status):
+    global fase, volume_atual
+    
+    indices = np.arange(frames) + fase
+    
+    outdata[:, 0] = volume_atual * np.sin(2 * np.pi * frequencia * indices / amostragem)
+    
+    fase += frames
+
+
 while(1):
     dados = porta.readline()
     print(dados.decode('utf-8'))
+
+    if dados < limiteSombra:
+        volume_atual = 2.0
+            with sd.OutputStream(channels=1, callback=audio_callback, samplerate=amostragem):
+    else:
+        volume_atual = 0.0
+    
